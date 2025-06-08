11083f88949928fa9cbbb9993d0cc1440e9eb0e3
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-06-08 15:23:13 +0530

    add CORS filter

---
+++ back-end/JavaEE-Final-Project-EMS-Backend/src/main/java/lk/ijse/javaEE/EMS/CORSFilter.java
@@ -0,0 +1,20 @@
+package lk.ijse.javaEE.EMS;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CORSFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        chain.doFilter(request, response);
+    }
+}
